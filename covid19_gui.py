006d6548951b98803c738f131c6806be2b4e41a0
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-10-10 17:50:01 -0400

    Initial commit

---
+++ covid19_gui.py
@@ -0,0 +1,21 @@
+import dotenv
+import os
+import requests
+
+_ = dotenv.load_dotenv()
+DC_USER = os.getenv('DC_USER')
+DC_PSWD = os.getenv('DC_PSWD')
+access_endpoint = "https://accounts.muckrock.com/api/token/"
+params = {'username': DC_USER, 'password': DC_PSWD}
+response = requests.post(access_endpoint, data = params )
+refresh_token = response.json()["refresh"]
+access_token = response.json()["access"]
+
+# download metadata
+DC_API = "https://api.www.documentcloud.org/api"
+DC_DOC = "https://www.documentcloud.org/documents/20793561-leopold-nih-foia-anthony-fauci-emails"
+# download list of documents belonging to the project
+search_endpoint = f"{DC_API}/oembed?url={DC_DOC}" 
+headers = {'Authorization': f'Bearer {access_token}'}
+response = requests.get(search_endpoint, headers=headers)
+print(response.json()['html'])
